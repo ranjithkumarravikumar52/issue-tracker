41a8db7f326a59d17302d143aea24ae0ebe029dd
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-02-19 13:37:42 -0500

    hello world

---
+++ src/main/java/issuetracker/config/DemoAppConfig.java
@@ -0,0 +1,25 @@
+package issuetracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("issuetracker")
+public class DemoAppConfig implements WebMvcConfigurer {
+
+
+	// define a bean for ViewResolver
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+		internalResourceViewResolver.setPrefix("/WEB-INF/view/");
+		internalResourceViewResolver.setSuffix(".jsp");
+		return internalResourceViewResolver;
+	}
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Ranjith
+  Date: 2/19/2019
+  Time: 1:25 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+    <body>
+        <p>Hello</p>
+    </body>
+</html>
