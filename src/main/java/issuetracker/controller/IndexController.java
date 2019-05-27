84e96cf118dca0a28664cad0ee21e6c0577d4bf9
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-05-27 12:19:45 +0530

    hello world navigation. #31.

--- src/main/java/issuetracker/controller/IndexController.java
+++ src/main/java/issuetracker/controller/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @GetMapping("/")
     public String showHome() {
-        return "home";
+        return "index";
     }
 
     @GetMapping("/oops")
