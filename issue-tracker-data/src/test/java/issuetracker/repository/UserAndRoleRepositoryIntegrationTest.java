5b73b35487d975b4609bbc9238cb644392c85add
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-06-01 16:40:24 +0530

    refactored and commented. Closes #27.

--- issue-tracker-data/src/test/java/issuetracker/repository/UserAndRoleRepositoryIntegrationTest.java
+++ issue-tracker-data/src/test/java/issuetracker/repository/UserAndRoleRepositoryIntegrationTest.java
@@ -2,21 +2,19 @@ package issuetracker.repository;
 
 import issuetracker.entity.Role;
 import issuetracker.entity.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 /**
  * <p>RunWith(SpringRunner.class) is used to provide a bridge between Spring Boot test features and JUnit. Whenever we are using any Spring Boot testing features in out JUnit tests, this annotation will be required.</p>
@@ -28,35 +26,55 @@ import static org.junit.Assert.assertNull;
  * <li>Turning on SQL logging</li>
  * <li>By default, data JPA tests are transactional and roll back at the end of each test.</li>
  * </ul>
+ *
+ * <br>
+ *     <p>This class primarily tests the relationship between User and Role and their bi-directional mapping. In addition, couple of methods on crud operation has been tested to validate our mapping of our entities.</p>
  */
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserAndRoleRepositoryIntegrationTest {
 
-    private static final Logger log = LoggerFactory.getLogger(UserAndRoleRepositoryIntegrationTest.class);
     @Autowired
     private UserRepository userRepository;
     @Autowired
     private RoleRepository roleRepository;
+
+    private User johnDoe;
+    private User janeDoe;
+    private Role developer;
+    private Role tester;
+
+    @Before
+    public void setup(){
+        //prep - save dev and tester roles
+        developer = Role.builder().name("developer").build();
+        tester = Role.builder().name("tester").build();
+        roleRepository.saveAll(Arrays.asList(developer, tester));
+
+        //prep - save john and jane users
+        johnDoe = User.builder().userName("johnDoe").password("pass123").email("johnDoe@gmail.com").firstName("john").lastName("doe").build();
+        janeDoe = User.builder().userName("janeDoe").password("pass456").email("janeDoe@gmail.com").firstName("jane").lastName("doe").build();
+        johnDoe.setRole(developer);
+        janeDoe.setRole(tester);
+        userRepository.saveAll(Arrays.asList(johnDoe, janeDoe));
+
+        //updating role->user direction
+        developer.getUserSet().add(johnDoe);
+        tester.getUserSet().add(janeDoe);
+        roleRepository.saveAll(Arrays.asList(developer, tester));
+
+    }
 
     /**
      * Test for navigation from user object to role object
      */
     @Test
     public void userToRoleObjectTreeNavigation() {
-        //when
-        User johnDoe = User.builder().userName("johnDoe").password("pass123").email("johnDoe@gmail.com").firstName("john").lastName("doe").build();
-        Role developer = Role.builder().name("developer").build();
-        johnDoe.setRole(developer);
-        userRepository.save(johnDoe);
-
-        //assertion checking object tree navigation from user to role
-        Optional<User> byId = userRepository.findById(1);
-        if (byId.isPresent()) {
-            System.out.println(byId.get().getUserName());
-            System.out.println(byId.get().getRole().getName());
-            assertEquals("developer", byId.get().getRole().getName());
-        }
+        //when - user with an associated role object is saved
+        //assertion - checking object tree navigation from user to role
+        User user = userRepository.findById(johnDoe.getId()).orElse(null);
+        assertNotNull(user);
+        assertEquals("developer", user.getRole().getName());
     }
 
     /**
@@ -64,38 +82,28 @@ public class UserAndRoleRepositoryIntegrationTest {
      */
     @Test
     public void roleToUserObjectTreeNavigation() {
-        Role tester = Role.builder().name("tester").build();
-        roleRepository.save(tester);
-
-        User janeDoe = User.builder().userName("janeDoe").password("pass456").email("janeDoe@gmail.com").firstName("jane").lastName("doe").build();
-        janeDoe.setRole(tester);
-        userRepository.save(janeDoe);
-
-        tester.getUserSet().add(janeDoe);
-        roleRepository.save(tester); //update
-
-        //navigation check
-        Optional<Role> roleRepositoryById = roleRepository.findById(1);
-        roleRepositoryById.ifPresent(role -> role.getUserSet().forEach(user -> {
-            log.info(user.getUserName());
-            assertEquals(tester.getName(), user.getRole().getName());
-        }));
+        //when - user and associated and role is saved
+        //assert - roleName and role->user->roles->role[0]->roleName
+        Role role = roleRepository.findById(tester.getId()).orElse(null);
+        assertNotNull(role);
+        assertEquals(tester.getName(), role.getUserSet().toArray(new User[0])[0].getRole().getName());
     }
 
     /**
-     * when only user object is saved then check that specific records exists
+     * when only user object is saved without saving role then check that specific records exists
      */
     @Test
     public void whenUserIsSaved_thenRecordExistsInUserRepo() {
-        //when
+        //when - only user object is saved without saving role then check that specific records exists
         User johnDoe = User.builder().userName("johnDoe").password("pass123").email("johnDoe@gmail.com").firstName("john").lastName("doe").build();
         Role developer = Role.builder().name("developer").build();
         johnDoe.setRole(developer);
         userRepository.save(johnDoe);
 
-        //then
-        Optional<User> optionalUser = userRepository.findById(1);
-        optionalUser.ifPresent(user -> assertEquals(johnDoe, user));
+        //assert - record saved and selected are same
+        User user = userRepository.findById(johnDoe.getId()).orElse(null);
+        assertNotNull(user);
+        assertEquals(johnDoe.getUserName(), user.getUserName());
     }
 
     /**
@@ -104,19 +112,25 @@ public class UserAndRoleRepositoryIntegrationTest {
      */
     @Test(expected = InvalidDataAccessApiUsageException.class)
     public void whenAnUserIsSaved_thenRoleFindAllThrowsException() {
+        //when - user is saved but the associated role is not saved
         User johnDoe = User.builder().userName("johnDoe").password("pass123").email("johnDoe@gmail.com").firstName("john").lastName("doe").build();
         Role developer = Role.builder().name("developer").build();
         johnDoe.setRole(developer);
         userRepository.save(johnDoe);
+
+        //assert - accessing role repo should throw exception
         roleRepository.findAll(); //throws exception
     }
 
     @Test(expected = InvalidDataAccessApiUsageException.class)
     public void whenAnUserIsSaved_thenUserFindAllThrowsException() {
+        //when - user is saved but the associated role is not saved
         User janeDoe = User.builder().userName("janeDoe").password("pass456").email("janeDoe@gmail.com").firstName("jane").lastName("doe").build();
         Role developer = Role.builder().name("developer").build();
         janeDoe.setRole(developer);
         userRepository.save(janeDoe);
+
+        //assert - accessing role repo should throw exception
         userRepository.findAll(); //throws exception
     }
 
@@ -125,17 +139,12 @@ public class UserAndRoleRepositoryIntegrationTest {
      */
     @Test
     public void whenUserAndRoleAreSaved_thenUserFindAllWorks() {
-        Role tester = Role.builder().name("tester").build();
-        roleRepository.save(tester);
-
-        User janeDoe = User.builder().userName("janeDoe").password("pass456").email("janeDoe@gmail.com").firstName("jane").lastName("doe").build();
-        janeDoe.setRole(tester);
-        userRepository.save(janeDoe);
-
+        //action - findAll
         List<User> userList = new ArrayList<>();
         userRepository.findAll().forEach(userList::add); //shouldn't throw an exception
-        assertEquals(1, userList.size());
 
+        //assert - only two records should exist
+        assertEquals(2, userList.size());
     }
 
     /**
@@ -143,38 +152,17 @@ public class UserAndRoleRepositoryIntegrationTest {
      */
     @Test
     public void whenAnUserIsDeleted_thenRoleShouldNotBeDeleted() {
-        //when
-        Role developer = Role.builder().name("developer").build();
-        roleRepository.save(developer);
-
-        User johnDoe = User.builder().userName("johnDoe").password("pass123").email("johnDoe@gmail.com").firstName("john").lastName("doe").build();
-        johnDoe.setRole(developer);
-        userRepository.save(johnDoe);
-
-        developer.getUserSet().add(johnDoe);
-        roleRepository.save(developer); //update bi-directional relationship
-
-        //action
+        //action - delete
         userRepository.delete(johnDoe);
 
-        //then
-        // 1. roleRepo should still hold dev role
-        Optional<Role> roleOptional = roleRepository.findById(developer.getId());
-        if (roleOptional.isPresent()) {
-            assertEquals(developer.getName(), roleOptional.get().getName());
-        } else {
-            throw new AssertionError("Not equals");
-        }
+        //assertion - 1. roleRepo should still hold dev role
+        Role role = roleRepository.findById(developer.getId()).orElse(null);
+        assertNotNull(role);
+        assertEquals(developer.getName(), role.getName());
 
-        //then
-        //2. user repo shouldn't hold john doe
-        Optional<User> userRepositoryByIdd = userRepository.findById(johnDoe.getId());
-        if (userRepositoryByIdd.isPresent()) {
-            throw new AssertionError("user failed to delete");
-        } else {
-            assertNull(userRepositoryByIdd.orElse(null));
-        }
-
+        //assertion - 2. user repo shouldn't hold john doe
+        User user = userRepository.findById(johnDoe.getId()).orElse(null);
+        assertNull(user);
 
     }
 
@@ -184,26 +172,12 @@ public class UserAndRoleRepositoryIntegrationTest {
      */
     @Test
     public void whenUserFindById_thenReturnAnUser() {
-        //prep
-        Role developer = Role.builder().name("developer").build();
-        roleRepository.save(developer);
-
-        User johnDoe = User.builder().userName("johnDoe").password("pass123").email("johnDoe@gmail.com").firstName("john").lastName("doe").build();
-        johnDoe.setRole(developer);
-        userRepository.save(johnDoe);
-
-        //update direction
-        developer.getUserSet().add(johnDoe);
-        roleRepository.save(developer);
-
-        //when
+        //action - findById
         User user = userRepository.findById(johnDoe.getId()).orElse(null);
-        if (user != null) {
-            log.info("User id by 1 exists");
-            assertEquals(johnDoe, user); //assertion
-        } else {
-            throw new AssertionError("User id by 1 doesn't exist"); //exception
-        }
+
+        //assert - findById should return user
+        assertNotNull(user);
+        assertEquals(johnDoe.getUserName(), user.getUserName());
     }
 
     /**
@@ -211,39 +185,17 @@ public class UserAndRoleRepositoryIntegrationTest {
      */
     @Test
     public void whenDeleteAnUser_thenDeleteSuccess() {
-        //prep
-        Role developer = Role.builder().name("developer").build();
-        roleRepository.save(developer);
-
-        User johnDoe = User.builder().userName("johnDoe").password("pass123").email("johnDoe@gmail.com").firstName("john").lastName("doe").build();
-        johnDoe.setRole(developer);
-        userRepository.save(johnDoe);
-
-        //update direction
-        developer.getUserSet().add(johnDoe);
-        roleRepository.save(developer);
-
-        //when
+        //action - delete
         userRepository.delete(johnDoe);
 
-        //assertion
-        User user = userRepository.findById(1).orElse(null);
-        if (user == null) {
-            log.info("User id by 1 deleted successfully");
-            assertNull(user); //assertion
-        } else {
-            throw new AssertionError("User id by 1 deletion failed"); //exception
-        }
+        //1. assertion - user should be deleted
+        User user = userRepository.findById(johnDoe.getId()).orElse(null);
+        assertNull(user);
 
-        //assertion
-        //role shouldn't be deleted
+        //2. assertion - role shouldn't be deleted
         Role role = roleRepository.findById(developer.getId()).orElse(null);
-        if (role != null) {
-            log.info("Role id by 1 is intact");
-            assertEquals(developer, role);
-        } else {
-            throw new AssertionError("Role shouldn't have been deleted");
-        }
+        assertNotNull(role);
+        assertEquals(developer.getName(), role.getName());
     }
 
     /**
@@ -251,38 +203,17 @@ public class UserAndRoleRepositoryIntegrationTest {
      */
     @Test
     public void whenDeleteAnRole_thenDeleteSuccess() {
-        //prep
-        Role developer = Role.builder().name("developer").build();
-        roleRepository.save(developer);
+        //action - delete role
+        roleRepository.delete(tester);
 
-        User johnDoe = User.builder().userName("johnDoe").password("pass123").email("johnDoe@gmail.com").firstName("john").lastName("doe").build();
-        johnDoe.setRole(developer);
-        userRepository.save(johnDoe);
+        //1. assertion - role should be deleted
+        Role role = roleRepository.findById(tester.getId()).orElse(null);
+        assertNull(role);
 
-        //update direction
-        developer.getUserSet().add(johnDoe);
-        roleRepository.save(developer);
-
-        //when - delete role
-        roleRepository.delete(developer);
-
-        //assertion for role
-        Role role = roleRepository.findById(1).orElse(null);
-        if (role == null) {
-            log.info("role id by 1 deleted successfully");
-            assertNull(role); //assertion
-        } else {
-            throw new AssertionError("role id by 1 deletion failed"); //exception
-        }
-
-        //assertion for user
-        User user = userRepository.findById(1).orElse(null);
-        if (user != null) {
-            log.info("user id by 1 is intact");
-            assertEquals(johnDoe, user);
-        } else {
-            throw new AssertionError("user shouldn't have been deleted");
-        }
+        //2. assertion - user shouldn't be deleted
+        User user = userRepository.findById(janeDoe.getId()).orElse(null);
+        assertNotNull(user);
+        assertEquals(janeDoe.getUserName(), user.getUserName());
 
     }
 
