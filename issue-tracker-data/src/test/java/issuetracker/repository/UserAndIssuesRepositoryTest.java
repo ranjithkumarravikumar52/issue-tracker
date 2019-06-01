20a5f9340d83551dfb52747d250b5f59ee3a22d0
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-06-02 00:07:49 +0530

    test data prep. #67.

---
+++ issue-tracker-data/src/test/java/issuetracker/repository/UserAndIssuesRepositoryTest.java
@@ -0,0 +1,83 @@
+package issuetracker.repository;
+
+import issuetracker.entity.Issue;
+import issuetracker.entity.PhoneNumber;
+import issuetracker.entity.Role;
+import issuetracker.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserAndIssuesRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private IssueRepository issueRepository;
+
+    //fields
+    private Role developer;
+    private Role tester;
+    private User johnDoe;
+    private User janeDoe;
+    private PhoneNumber phoneNumber1;
+    private PhoneNumber phoneNumber2;
+    private Issue blockerIssue;
+    private Issue graphicsIssue;
+
+    @Before
+    public void setUp() throws Exception {
+        developer = Role.builder().name("developer").build();
+        tester = Role.builder().name("tester").build();
+        roleRepository.saveAll(Arrays.asList(developer, tester));
+
+        //prep - get two users
+        johnDoe = User.builder().userName("johnDoe").password("pass123").email("johnDoe@gmail.com").firstName("john").lastName("doe").build();
+        janeDoe = User.builder().userName("janeDoe").password("pass456").email("janeDoe@gmail.com").firstName("jane").lastName("doe").build();
+
+        //prep - set role relationship with each user
+        johnDoe.setRole(developer);
+        janeDoe.setRole(tester);
+
+        //prep - get 2 phone numbers
+        phoneNumber1 = PhoneNumber.builder().phoneNumber("1234567890").build();
+        phoneNumber2 = PhoneNumber.builder().phoneNumber("6789012345").build();
+
+        //prep - set phone relationship with each user
+        johnDoe.setPhoneNumber(phoneNumber1);
+        janeDoe.setPhoneNumber(phoneNumber2);
+
+        //save user
+        userRepository.saveAll(Arrays.asList(johnDoe, janeDoe));
+
+        //bi-directional relationship with user and role
+        developer.getUserSet().add(johnDoe);
+        tester.getUserSet().add(janeDoe);
+
+        //update for the bi-directional relationship
+        roleRepository.saveAll(Arrays.asList(developer, tester));
+
+        //create 2 issues with john and jane defaults to posted by in each of them
+        blockerIssue = Issue.builder().issueDescription("blocker issue").postedBy(johnDoe).build();
+        graphicsIssue = Issue.builder().issueDescription("graphics issue").postedBy(janeDoe).build();
+        issueRepository.saveAll(Arrays.asList(blockerIssue, graphicsIssue));
+    }
+
+    @Test
+    public void sanityCheck(){
+        assertEquals(2, userRepository.count());
+        assertEquals(2, roleRepository.count());
+        assertEquals(2, issueRepository.count());
+    }
+}
