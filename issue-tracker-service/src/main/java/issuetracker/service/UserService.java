0266036b785e25d191e90374ad3596a3cf39ce78
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-06-05 16:22:41 +0530

    refactored old method in user service and implemented findPaginated(). #10.

--- issue-tracker-service/src/main/java/issuetracker/service/UserService.java
+++ issue-tracker-service/src/main/java/issuetracker/service/UserService.java
@@ -1,6 +1,9 @@
 package issuetracker.service;
 
 import issuetracker.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface UserService extends CrudService<User, Integer>{
+    Page<User> findPaginated(Pageable pageable); //TODO once all entities implement this method, refactor this method to crudservice if needed
 }
