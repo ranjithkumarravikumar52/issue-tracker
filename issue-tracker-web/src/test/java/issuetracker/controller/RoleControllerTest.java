6a6a1ffa6ad87f88fc09bf8c91a106996d139ed1
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-06-21 19:19:59 +0530

    added admin credentials to mock user. Closes #131.

--- issue-tracker-web/src/test/java/issuetracker/controller/RoleControllerTest.java
+++ issue-tracker-web/src/test/java/issuetracker/controller/RoleControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(RoleController.class)
+@WithMockUser(username = "ranjith@gmail.com", password = "password", authorities = "admin")
 public class RoleControllerTest extends AbstractSecuredUserControllerTest{
 
     @Autowired
