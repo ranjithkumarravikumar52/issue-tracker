d4dd6f529cd9ff41ee43f989f50e5016ee7ca1f2
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-06-21 18:44:56 +0530

    injected authentication service in index. #119.

--- issue-tracker-web/src/main/java/issuetracker/controller/IndexController.java
+++ issue-tracker-web/src/main/java/issuetracker/controller/IndexController.java
@@ -1,14 +1,32 @@
 package issuetracker.controller;
 
+import issuetracker.entity.User;
+import issuetracker.service.AuthenticationFacadeService;
+import issuetracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("")
 public class IndexController {
+
+    private final AuthenticationFacadeService authenticationFacadeService;
+    private final UserService userService;
+
+    @Autowired
+    public IndexController(AuthenticationFacadeService authenticationFacadeService, UserService userService) {
+        this.authenticationFacadeService = authenticationFacadeService;
+        this.userService = userService;
+    }
+
     @GetMapping("/")
-    public String showHome() {
+    public String showHome(Model model) {
+        String loggedInUserEmailId = authenticationFacadeService.getAuthentication().getName();//this will give us current logged in user email id
+        User userByEmail = userService.findUserByEmail(loggedInUserEmailId);
+        model.addAttribute("user", userByEmail);
         return "home";
     }
 
