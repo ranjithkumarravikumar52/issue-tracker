17f592a98a491f5ff48fad3d59075908e5fb5689
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-06-26 03:03:46 +0530

    removed dataloader value from app-dev and updated devdataloader. #136.

--- issue-tracker-web/src/main/java/issuetracker/bootstrap/DevDataLoader.java
+++ issue-tracker-web/src/main/java/issuetracker/bootstrap/DevDataLoader.java
@@ -1,82 +1,48 @@
 package issuetracker.bootstrap;
 
-import issuetracker.entity.Project;
-import issuetracker.entity.Role;
-import issuetracker.entity.User;
+import issuetracker.entity.*;
 import issuetracker.service.IssueService;
 import issuetracker.service.ProjectService;
 import issuetracker.service.RoleService;
 import issuetracker.service.UserService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Component
 @Profile("dev")
 public class DevDataLoader implements CommandLineRunner {
 
-    @Value("#{new Integer('${dataloader.value}')}")
-    private Integer FAKE_USER_DATA_COUNT;
-
-    private static final String[] LOCALE_LIST = {"de-CH", "en-GB", "en-IND", "en-PAK", "en-US"};
+    //static issue id counter
+    private static int issueCounter = 1;
 
     private final UserService userService;
     private final IssueService issueService;
     private final ProjectService projectService;
     private final RoleService roleService;
+
+    //5 roles
     private Role admin;
     private Role developer;
     private Role tester;
     private Role sales;
     private Role humanResources;
 
+    //3 projects
     private Project mobileApp;
     private Project machineLearningDemo;
     private Project nlpProject;
-    private User adminUser = User.builder()
-            .userName("rk1234")
-            .password("password")
-            .email("ranjith@gmail.com")
-            .firstName("ranjith")
-            .lastName("kumar")
-            .image(null)
-            .build();
-    private User devUser = User.builder()
-            .userName("dev123")
-            .password("password")
-            .email("dev@gmail.com")
-            .firstName("john")
-            .lastName("dev")
-            .image(null)
-            .build();
-    private User testerUser = User.builder()
-            .userName("test12")
-            .password("password")
-            .email("tester@gmail.com")
-            .firstName("john")
-            .lastName("tester")
-            .image(null)
-            .build();
-    private User salesUser = User.builder()
-            .userName("sales1")
-            .password("password")
-            .email("sales@gmail.com")
-            .firstName("john")
-            .lastName("sales")
-            .image(null)
-            .build();
-    private User hrUser = User.builder()
-            .userName("hr1234")
-            .password("password")
-            .email("hr@gmail.com")
-            .firstName("john")
-            .lastName("hr")
-            .image(null)
-            .build();
 
+    //5 users
+    private User adminUser;
+    private User devUser;
+    private User testerUser;
+    private User salesUser;
+    private User hrUser;
 
     public DevDataLoader(UserService userService, IssueService issueService, ProjectService projectService,
                          RoleService roleService) {
@@ -90,6 +56,21 @@ public class DevDataLoader implements CommandLineRunner {
     public void run(String... args) {
         initProjectsData();
         initRolesData();
+        createTestUserWithRoles();
+
+        //2 lists - project and user for init issues and 1 list for issue to user saveAll() on issues
+        List<Project> projectList = new ArrayList<>(projectService.findAll());
+        List<User> userList = new ArrayList<>(userService.findAll());
+
+        //each user post one open and one closed issue, for all the projects
+        for(User user: userList){
+            for(Project project : projectList){
+                Issue issueOpen = initOpenIssuesData(user, project);
+                Issue issueClosed = initClosedIssuesData(user, project);
+                issueService.saveAll(Arrays.asList(issueOpen, issueClosed));
+                projectService.save(project); //update project with issue relationship
+            }
+        }
     }
 
     private void initProjectsData() {
@@ -107,6 +88,7 @@ public class DevDataLoader implements CommandLineRunner {
                 .projectDescription("Social media sentiment analysis")
                 .build();
 
+        //save all the 3 projects
         projectService.saveAll(Arrays.asList(mobileApp, machineLearningDemo, nlpProject));
 
     }
@@ -129,20 +111,98 @@ public class DevDataLoader implements CommandLineRunner {
                 .name("human resources")
                 .build();
 
+        //save all 5 roles
         roleService.saveAll(Arrays.asList(developer, tester, admin, sales, humanResources));
 
     }
 
     private void createTestUserWithRoles() {
+        adminUser = User.builder()
+                .userName("rk1234")
+                .password("password")
+                .email("ranjith@gmail.com")
+                .firstName("ranjith")
+                .lastName("kumar")
+                .image(null)
+                .build();
+        adminUser.setRole(admin);
+        adminUser.addProject(machineLearningDemo);
+        adminUser.addProject(nlpProject);
+        adminUser.addProject(mobileApp);
+
+        devUser = User.builder()
+                .userName("dev123")
+                .password("password")
+                .email("dev@gmail.com")
+                .firstName("john")
+                .lastName("dev")
+                .image(null)
+                .build();
+        devUser.setRole(developer);
+        devUser.addProject(nlpProject);
+        devUser.addProject(mobileApp);
+
+        testerUser = User.builder()
+                .userName("test12")
+                .password("password")
+                .email("tester@gmail.com")
+                .firstName("john")
+                .lastName("tester")
+                .image(null)
+                .build();
+        testerUser.setRole(tester);
+        testerUser.addProject(nlpProject);
+        testerUser.addProject(mobileApp);
+
+        salesUser = User.builder()
+                .userName("sales1")
+                .password("password")
+                .email("sales@gmail.com")
+                .firstName("john")
+                .lastName("sales")
+                .image(null)
+                .build();
+        salesUser.setRole(sales);
+        salesUser.addProject(nlpProject);
+        salesUser.addProject(mobileApp);
+
+        hrUser = User.builder()
+                .userName("hr1234")
+                .password("password")
+                .email("hr@gmail.com")
+                .firstName("john")
+                .lastName("hr")
+                .image(null)
+                .build();
+        hrUser.setRole(humanResources);
+        hrUser.addProject(nlpProject);
+        hrUser.addProject(mobileApp);
+
+        userService.saveAll(Arrays.asList(adminUser, devUser, testerUser, salesUser, hrUser));
+        roleService.saveAll(Arrays.asList(developer, tester, admin, sales, humanResources)); //update roles relationship
+        projectService.saveAll(Arrays.asList(mobileApp, machineLearningDemo, nlpProject)); //update project relationship
 
     }
 
-    private User initUsersData() {
-        return null;
+    private Issue initOpenIssuesData(User user, Project project) {
+        return Issue.builder()
+                .title("open issue title " + issueCounter)
+                .issueDescription("open issue description " + issueCounter++)
+                .openedBy(user)
+                .closedBy(null)
+                .issueStatus(IssueStatus.OPEN)
+                .project(project)
+                .build();
     }
-
-    private void initIssuesData(User user) {
-
+    private Issue initClosedIssuesData(User user, Project project) {
+        return Issue.builder()
+                .title("closed issue title " + issueCounter)
+                .issueDescription("closed issue description " + issueCounter++)
+                .openedBy(user)
+                .closedBy(user)
+                .issueStatus(IssueStatus.CLOSED)
+                .project(project)
+                .build();
     }
 
 
